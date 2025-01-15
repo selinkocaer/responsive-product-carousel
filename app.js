efa48c460b9947e964f56fd6a7df9d04fd69366e
Selin Kocaer <72472012+selinkocaer@users.noreply.github.com>
2025-01-15 16:40:20 +0300

    Update app.js
    API delete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function () {
-    const PRODUCTS_URL = "https://gist.githubusercontent.com/sevindi/5765c5812bbc8238a38b3cf52f233651/raw/56261d81af8561bf0a7cf692fe572f9e1e91f372/products.json";
+    const PRODUCTS_URL = Your API;
     const CAROUSEL_TITLE = "You Might Also Like";
 
     const fetchProducts = async () => {
